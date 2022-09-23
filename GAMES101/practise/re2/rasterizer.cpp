8d17de611857da871285d142a0f662137b218dcf
lys5588 <yunshanli2001@gmail.com>
2022-09-23 20:00:10 +0800

    fin re2

--- GAMES101/practise/re2/rasterizer.cpp
+++ GAMES101/practise/re2/rasterizer.cpp
@@ -148,7 +148,7 @@ void rst::rasterizer::rasterize_triangle(const Triangle& t) {
 
     for(int i = l_sc;i<r_sc;i++){
         for(int j=b_sc;j<t_sc;j++){
-            std::cout<<"point"<<i<<j<<std::endl;
+            // std::cout<<"point"<<i<<j<<std::endl;
             if(insideTriangle(i+0.5,j+0.5,t.v)){
                 // std::cout<<"hello"<<std::endl;
                 //compute the interpolation result of z
