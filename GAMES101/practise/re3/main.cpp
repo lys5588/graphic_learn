ca5debd589045b9186b8485775e446a9a9156139
lys5588 <yunshanli2001@gmail.com>
2022-09-25 00:56:18 +0800

    work on re3

--- GAMES101/practise/re3/main.cpp
+++ GAMES101/practise/re3/main.cpp
@@ -221,8 +221,8 @@ Eigen::Vector3f phong_fragment_shader(const fragment_shader_payload& payload)
 
         
         std::cout<<"a"<<ambient<<std::endl;
-        std::cout<<"b"<<ambient<<std::endl;
-        std::cout<<"c"<<ambient<<std::endl;
+        std::cout<<"b"<<diffuse<<std::endl;
+        std::cout<<"c"<<specular<<std::endl;
         std::cout<<"res1"<<result_color+(ambient,diffuse,specular)<<std::endl;
         std::cout<<"res2"<<result_color+((ambient+diffuse+specular))<<std::endl;
         result_color+=(ambient+diffuse+specular);
