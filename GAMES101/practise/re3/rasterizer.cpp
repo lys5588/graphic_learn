7aa582b2884116661258c96529deb3714f6820e0
lys5588 <yunshanli2001@gmail.com>
2022-09-24 15:07:59 +0800

    work on re3

--- GAMES101/practise/re3/rasterizer.cpp
+++ GAMES101/practise/re3/rasterizer.cpp
@@ -293,9 +293,9 @@ void rst::rasterizer::rasterize_triangle(const Triangle& t, const std::array<Eig
                     auto interpolated_texcoords=interpolate(alpha,beta,gamma,t.tex_coords[0],t.tex_coords[1],t.tex_coords[2],1);
                     auto interpolated_shadingcoords=interpolate(alpha,beta,gamma,view_pos[0],view_pos[1],view_pos[2],1);
 
-    //                 fragment_shader_payload payload( interpolated_color, interpolated_normal.normalized(), interpolated_texcoords, texture ? &*texture : nullptr);
-    //                 payload.view_pos = interpolated_shadingcoords;
-    //                 auto pixel_color = fragment_shader(payload);  
+                    fragment_shader_payload payload( interpolated_color, interpolated_normal.normalized(), interpolated_texcoords, texture ? &*texture : nullptr);
+                    payload.view_pos = interpolated_shadingcoords;
+                    auto pixel_color = fragment_shader(payload);  
 
                     
     //                 set_pixel(Eigen::Vector2f(i,j),pixel_color);
