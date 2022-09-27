4d66f36819859ef0fb431e1183af5062f25a0a37
lys5588 <yunshanli2001@gmail.com>
2022-09-27 19:40:36 +0800

    work on re4

--- GAMES101/practise/re4/main.cpp
+++ GAMES101/practise/re4/main.cpp
@@ -83,7 +83,7 @@ int main()
             cv::circle(window, point, 3, {255, 255, 255}, 3);
         }
 
-        if (control_points.size()==4) 
+        if (key==13) 
         {
             // naive_bezier(control_points, window);
             bezier(control_points, window);
