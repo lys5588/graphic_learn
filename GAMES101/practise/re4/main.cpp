d9db35cc5994bf90c50d2874d6aa362103eef731
lys5588 <yunshanli2001@gmail.com>
2022-09-27 19:32:24 +0800

    work on re4

--- GAMES101/practise/re4/main.cpp
+++ GAMES101/practise/re4/main.cpp
@@ -37,11 +37,11 @@ cv::Point2f recursive_bezier(const std::vector<cv::Point2f> &control_points, flo
     std::vector<cv::Point2f> round_point_list={control_points.begin(),control_points.end()};
     std::vector<cv::Point2f> temp_point_list;
     for(int i=0;i<point_num-1;i++){
-        std::cout<<"handle point num"<<i<<std::endl;
-        for(int j=0;i<point_num-i-1;j++){
+        // std::cout<<"handle point num"<<i<<std::endl;
+        for(int j=0;j<point_num-i-1;j++){
             cv::Point2f p1=round_point_list[j];
             cv::Point2f p2=round_point_list[j+1];
-            std::cout<<"temp point"<<p1.x<<p1.y<<p2.x<<p2.y<<std::endl;
+            // std::cout<<"temp point"<<p1.x<<p1.y<<p2.x<<p2.y<<std::endl;
             cv::Point2f p_temp(p1.x*t+p2.x*(1-t),p1.y*t+p2.y*(1-t));
             temp_point_list.emplace_back(p_temp);
         }
