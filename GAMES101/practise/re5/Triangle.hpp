1ce5d0a5ae2c9e05739a13b6792498662161735a
lys5588 <yunshanli2001@gmail.com>
2022-10-12 16:35:58 +0800

    fix re5

--- GAMES101/practise/re5/Triangle.hpp
+++ GAMES101/practise/re5/Triangle.hpp
@@ -14,7 +14,7 @@ bool rayTriangleIntersect(const Vector3f& v0, const Vector3f& v1, const Vector3f
     auto S2 = crossProduct(S,E1);
 
     float S1E1 = dotProduct(S1,E1);
-    float t = dotProduct(S1,E2) / S1E1;
+    float t = dotProduct(S2,E2) / S1E1;
     float b1 = dotProduct(S1,S) / S1E1;
     float b2 = dotProduct(S2,dir) / S1E1;
 
@@ -33,32 +33,7 @@ bool rayTriangleIntersect(const Vector3f& v0, const Vector3f& v1, const Vector3f
     return false;
 }
 
-// bool rayTriangleIntersect(const Vector3f& v0, const Vector3f& v1, const Vector3f& v2, const Vector3f& orig,
-//                           const Vector3f& dir, float& tnear, float& u, float& v)
-// {   
-//     // TODO: Implement this function that tests whether the triangle
-//     // that's specified bt v0, v1 and v2 intersects with the ray (whose
-//     // origin is *orig* and direction is *dir*)
-//     // Also don't forget to update tnear, u and v.
 
-//     auto e1 = v1 - v0, e2 = v2 - v0, s = orig - v0;
-//     auto s1 = crossProduct(dir, e2), s2 = crossProduct(s, e1);
-
-//     float t = dotProduct(s2, e2) / dotProduct(s1, e1);
-//     float b1 = dotProduct(s1, s) / dotProduct(s1, e1);
-//     float b2 = dotProduct(s2, dir) / dotProduct(s1, e1);
-
-//     if (t > 0.0 && b1 > 0.0 && b2 > 0.0 && (1 - b1 - b2) > 0.0)
-//     {
-//         tnear = t;
-//         u = b1;
-//         v = b2;
-        
-//         return true;
-//     }
-
-//     return false;
-// }
 
 class MeshTriangle : public Object
 {
